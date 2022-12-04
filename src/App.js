d129447e62ddca3ae696050d3718b7cbb5fe3457
mkandan1 <64264387+mkandan1@users.noreply.github.com>
2022-12-04 17:24:49 +0530

    v1.0 [Fix]

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import Home from './Home';
 import React from 'react';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import { Component } from 'react';
-import Signup from './components/admin/Users/Signup/Signup';
 import Login from './components/Oauth/Login/Login';
-import Trendings from './components/Trendins/trendins'
+import Trendings from './components/Trendings/trendings'
 import Privacy from './components/privacy-terms/Privacy';
 import Terms from './components/privacy-terms/Terms';
 import Test from './components/Test/Test';
-import AdminHome from './components/admin/AdminHome';
-import AdminLogin from './components/admin/Oauth/AdminLogin';
 
 class App extends Component {
   render() {
@@ -20,13 +17,10 @@ class App extends Component {
            <Routes>
                  <Route exact path='/' element={< Home />}></Route>
                  <Route exact path='/Oauth/Login' element={< Login />}></Route>
-                 <Route exact path='/platform/admin/createUser' element={< Signup />}></Route>
                  <Route exact path='/trendings' element={< Trendings />}></Route>
                  <Route exact path='/pigama/terms' element={< Terms />}></Route>
                  <Route exact path='/test' element={< Test />}></Route>
                  <Route exact path='/pigama/privacy' element={< Privacy />}></Route>
-                 <Route exact path='/platform/admin/' element={< AdminHome />}></Route>
-                 <Route exact path='/platform/admin/Oauth/Login' element={< AdminLogin />}></Route>
           </Routes>
           </div>
        </Router>
