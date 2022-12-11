3e266ccde14ad36587e4d63bf53958c329ae200c
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2022-12-11 18:16:19 +0530

    Version 1 [BF #002]

---
+++ build/precache-manifest.1432878261c76079f5012c6367b36496.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "89114475ed48d7a454e7",
+    "url": "./static/css/main.300b73ff.chunk.css"
+  },
+  {
+    "revision": "89114475ed48d7a454e7",
+    "url": "./static/js/main.6ca9a2c4.chunk.js"
+  },
+  {
+    "revision": "b0c4f2b6b50102d041cc",
+    "url": "./static/js/runtime~main.3efd95e3.js"
+  },
+  {
+    "revision": "b117e70f6b8b3671b336",
+    "url": "./static/js/2.003c1527.chunk.js"
+  },
+  {
+    "revision": "59e4468e193d69f80c99",
+    "url": "./static/js/3.c390db1f.chunk.js"
+  },
+  {
+    "revision": "f003b94b4a8318f2874e77cf9c163a33",
+    "url": "./static/media/bg_hero.f003b94b.png"
+  },
+  {
+    "revision": "c95786e2d75397492f5b80bc81824be9",
+    "url": "./static/media/Article_1.c95786e2.png"
+  },
+  {
+    "revision": "cb39b3f26a5942d95dc79168933ca71c",
+    "url": "./static/media/Article_2.cb39b3f2.png"
+  },
+  {
+    "revision": "de3b4fa4dbf80d6a683576027a8d10c7",
+    "url": "./index.html"
+  }
+];
