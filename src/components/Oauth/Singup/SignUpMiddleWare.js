b5d02e9d5e4f8ece0bc06e65b87c091ac1a2d9d1
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2022-12-14 15:35:11 +0530

    Update SignUpMiddleWare.js

--- src/components/Oauth/Singup/SignUpMiddleWare.js
+++ src/components/Oauth/Singup/SignUpMiddleWare.js
@@ -4,7 +4,7 @@ import Signup from './Signup'
 function SignUpMiddleWare() {
     return (
         <div>{
-            false ? <Signup /> : <div>
+            true ? <Signup /> : <div>
                 <p className='text-center mt-5'>Currently we are in beta testing</p>
                 <h4 className='text-center'>Account creation is disabled</h4>
             </div>
