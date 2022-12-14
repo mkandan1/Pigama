678bbd9cc956c11dbc8b31874758efa1366c1b92
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2022-12-14 13:22:17 +0530

    Update VideoEdting2022.js

--- src/components/Trendings/articles/VideoEdting2022.js
+++ src/components/Trendings/articles/VideoEdting2022.js
@@ -63,7 +63,7 @@ class VideoEditing2022 extends Component {
         const auth = getAuth(config);
         onAuthStateChanged(auth, (user) => {
             const uid = user.uid;
-            const creditAmount = 0.08
+            const creditAmount = 0.20
 
             const data = { uid: uid, creditAmount: creditAmount }
 
