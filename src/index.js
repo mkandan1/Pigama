b1dda9417ec43b3b92c4c27955648094c2d58c11
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2022-12-15 21:00:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   {false ? <App/> : <Status />}
+   {true ? <App/> : <Status />}
   </React.StrictMode>
 );
 
